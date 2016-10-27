5e633f9ba48eba7a50bf4c725828113d56152807
Joe Anzalone <joe@shmit.com>
2016-10-27 09:29:14 -0400

    Generate Users table with laracasts/Laravel-5-Generators-Extended:     php artisan make:migration:schema create_users_table --schema="\     facebook_id:string:unique,     name:string:nullable,     role_id:integer:unsigned:default(0),     rsvp_status_id:integer:unsigned:default(0)"

---
+++ database/migrations/2016_10_27_132829_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('facebook_id')->unique();
+            $table->string('name')->nullable();
+            $table->integer('role_id')->unsigned()->default(0);
+            $table->integer('rsvp_status_id')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
