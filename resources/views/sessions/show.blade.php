8d6a3166a6ea0c68a24656340b14b0d59b231f47
Joe Anzalone <joe@shmit.com>
2016-10-27 14:11:34 -0400

    Display time and location on session page, if available

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -1,4 +1,20 @@
 <h1>{{ $session->title }}</h1>
 <div>Category: {{ $session->category }}</div>
 <div>Owner: {{ $session->user->name }}</div>
+
+
+@if ($session->time)
+<div>Time: {{ $session->time }}</div>
+@endif
+
+@if ($session->location)
+<div>Where: {{ $session->location }}</div>
+@endif
+
+@if (strtolower($session->category) === 'other')
+<h2>About this session</h2>
+@else
+<h2>About this {{ strtolower($session->category) }}</h2>
+@endif
+
 <div>{{ $session->description }}</div>
