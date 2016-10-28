a10506b3d880b6b16fba5d4f2ec5c923b8c1a2da
Joe Anzalone <joe@shmit.com>
2016-10-28 10:03:36 -0400

    Add more words to welcome page and style login button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,8 @@
 <h1>Welcome!</h1>
-<p>
-    <a href="/login">Login with Facebook to continue</a>
-</p>
+
+<p>To view the unconference's sessions, or to list your own, you must first RSVP to the Facebook event.</p>
+<p>Once you've done that, come back here and log in with Facebook.</p>
+
+<div class="facebook-login-button">
+    <a href="{{ route('facebook.login') }}">Log in with Facebook to continue</a>
+</div>
