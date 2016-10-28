98cd6674b0e7bdcf9b896ae1cf8790707411ace1
Joe Anzalone <joe@shmit.com>
2016-10-28 00:51:06 -0400

    Style the session edit screen a bit

--- resources/views/sessions/edit.blade.php
+++ resources/views/sessions/edit.blade.php
@@ -11,7 +11,6 @@
         <option {{ $session->category_id === 0 ? 'selected' : ''}} value="0">Other</option>
     </select>
     <input name="session[location]" type="text" placeholder="Location" value="{{ $session->location }}">
-    <textarea name="session[description]" placeholder="Description">{{ $session->description }}</textarea>
 
     @can('set-time', \App\Session::class)
         <input name="session[time]" type="time" placeholder="Time" value="{{ $session->time }}">
@@ -24,6 +23,8 @@
         @endforeach
         </select>
     @endcan
+
+    <textarea name="session[description]" placeholder="Description">{{ $session->description }}</textarea>
 
     <button type="submit">Save</button>
 </form>
